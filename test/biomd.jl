96484c8381b68993d3ca2dfff76cc537ba2f7c36
anand jain <anandj@uchicago.edu>
2021-07-18 15:06:23 -0400

    cleanup and fixes. limiting test counts so that CI will run and actually complete

--- test/biomd.jl
+++ test/biomd.jl
@@ -1,14 +1,16 @@
+N = 100
 # get the dataset
-# df = biomodels(;curl_meta=true, verbose=false)
+df = biomodels(;curl_meta=true, verbose=false, limit=N)
 
 println("BIOMD DATASET TESTING")
-biomd_fns = get_biomd_fns(;ode=true)
+biomd_fns = get_biomd_fns()
 fn = biomd_fns[1]
-@test length(biomd_fns) == 984
 @test readSBML(fn) isa SBML.Model
 
 now_fmtd = Dates.format(now(), dateformat"yyyy-mm-dd\THH-MM-SS")
-biomd_df = lower_fns(biomd_fns[1:50]; write_fn="biomd_odes_lowered_$(now_fmtd).csv", verbose=true)
+biomd_df = lower_fns(biomd_fns; write_fn="biomd_odes_lowered_$(now_fmtd).csv", verbose=true)
 sort!(biomd_df, [:n_dvs, :n_ps], rev=true)
 @show biomd_df
-@info nrow(filter(retcode => x-> x==5)) "good ones"
+
+num_good = nrow(filter(:retcode => x-> x==5, biomd_df)) 
+@info "biomd num_good: $num_good / $N"
