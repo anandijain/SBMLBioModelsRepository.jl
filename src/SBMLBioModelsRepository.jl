580267995f5baedcd9ab52185c5f02ee8033ea97
anand jain <anandj@uchicago.edu>
2021-07-19 11:36:23 -0400

    add expected_error column to lower_fns df fix exports

--- src/SBMLBioModelsRepository.jl
+++ src/SBMLBioModelsRepository.jl
@@ -16,13 +16,8 @@ include("lower.jl")
 include("biomd.jl")
 include("suite.jl")
 
-# "do it for me" functions
-export sbml_test_suite, biomodels
-
-export curl_biomd_xmls
-export curl_biomd_metadata, biomd_metadata, curl_biomd_zips, biomd_zip_urls, unzip_biomd
-export get_sbml_suite_fns, jsonfn_to_df, get_biomd_fns
-export setup_settings_txt, verify_case, verify_all
+export jsonfn_to_df, curl_biomd_xmls, curl_biomd_metadata, read_biomd_metadf, biomodels
+export sbml_test_suite, get_sbml_suite_fns, setup_settings_txt, verify_case, verify_all, verify_plot
 export datadir, logdir
 export lower_one, lower_fns
 
--- src/lower.jl
+++ src/lower.jl
@@ -1,16 +1,23 @@
+const expected_errs = 
+    ["SBML files with rules are not supported",
+    "Model contains no reactions.",
+    "are not yet implemented.",
+    "Please make reaction irreversible or rearrange kineticLaw to the form `term1 - term2`."]
+
 function lower_one(fn, df; verbose=false)
-    expected_errs = ["SBML files with rules are not supported", "Model contains no reactions."]
     k = 0
     n_dvs = 0
     n_ps = 0
     time = 0.0
     diffeq_retcode = :nothing
+    expected_err = false
     err = ""
     try
+        SBMLToolkit.checksupport(fn)
         ml = SBML.readSBML(fn, doc -> begin
-                set_level_and_version(3, 2)(doc)
-                convert_simplify_math(doc)
-                end)
+            set_level_and_version(3, 2)(doc)
+            convert_simplify_math(doc)
+        end)
         k = 1
         rs = ReactionSystem(ml)
         k = 2
@@ -20,7 +27,7 @@ function lower_one(fn, df; verbose=false)
         k = 3
         prob  = ODEProblem(sys, Pair[], (0, 1.))
         k = 4
-        sol = solve(prob, TRBDF2(), dtmax=0.5; force_dtmin=false, unstable_check=unstable_check = (dt,u,p,t) -> any(isnan, u))
+        sol = solve(prob, TRBDF2(), dtmax=0.5; force_dtmin=false, unstable_check=unstable_check = (dt, u, p, t) -> any(isnan, u))
         diffeq_retcode = sol.retcode
         if diffeq_retcode == :Success
             k = 5
@@ -29,19 +36,19 @@ function lower_one(fn, df; verbose=false)
     catch e
         err = string(e)
         if sum([occursin(e, err) for e in expected_errs]) > 0
-            err = "Expected error: "*err
+            expected_err = true
         end
         if length(err) > 1000 # cutoff since I got ArgumentError: row size (9088174) too large 
             err = err[1:1000]
         end
     finally
-        push!(df, (fn, k, n_dvs, n_ps, time, err, diffeq_retcode))
+        push!(df, (fn, k, n_dvs, n_ps, time, err, expected_err, diffeq_retcode))
         verbose && @info("$(basename(fn)) done with a code $k and error msg: $err")
     end
 end
 
 function lower_fns(fns; verbose=false, write_fn=nothing)
-    df = DataFrame(file=String[], retcode=Int[], n_dvs=Int[], n_ps=Int[], time = Float64[], error=String[], diffeq_retcode=Symbol[])
+    df = DataFrame(file=String[], retcode=Int[], n_dvs=Int[], n_ps=Int[], time=Float64[], error=String[], expected_error=Bool[], diffeq_retcode=Symbol[])
     for fn in fns 
         lower_one(fn, df; verbose=verbose)
     end
