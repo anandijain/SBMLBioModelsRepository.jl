074ff9ae2d8f91c44f26c1dd11be98593bcd77ed
paulflang <paul.lang@wolfson.ox.ac.uk>
2022-05-29 21:59:12 +0100

    send cases 600:700 to ci

--- test/test_suite.jl
+++ test/test_suite.jl
@@ -2,7 +2,7 @@
 sbml_test_suite()
 
 # cases = [23]  #1295:1296  # max: 1664
-cases = [600,700...]
+cases = [600:700...]
 println("****SBML TEST SUITE TESTING****")
 suite_fns = get_sbml_suite_fns()[cases]
 fn = suite_fns[1]
